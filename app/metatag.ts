afe51588df3db8d2567438e2da525e2c935c23c1
ake <tamiratake@gmail.com>
2025-01-02 14:30:54 +0300

    og stuff

---
+++ app/metatag.ts
@@ -0,0 +1,5 @@
+export default {
+    title: "UI-Forest - Empower the use of styled and animated components based on aceternity and shadcn",
+    desc: "Take web design to the next level for modern  user exprience with UI-Forest",
+    ogImage: "og.png",
+  };
