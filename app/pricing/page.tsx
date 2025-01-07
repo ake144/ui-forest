79447e2a1817e24bbb2e71ca794f37216a66f638
Aklilu Tamirat <122739445+ake144@users.noreply.github.com>
2025-01-07 08:41:51 +0300

    Update page.tsx

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -4,7 +4,6 @@ import { Check } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import Navbar from '@/components/main-nav'
-import Link from "next/link";
 
 export default function PricingPage() {
   const features = [
@@ -71,14 +70,15 @@ export default function PricingPage() {
             <p className="text-zinc-400 mb-4">
               Join thousands of developers and designers who trust UI Forest for their projects
             </p>
-            <Link href='t.me/AkeTame'>
-            <Button 
-                variant="outline" 
-                className="w-full border-purple-800 hover:text-md sm:w-auto"
-                >
-                Purchase
-                </Button>
-           </Link>
+            <a 
+                  href="https://t.me/AkeTame" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="w-full border border-purple-800 hover:text-md sm:w-auto inline-flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium text-purple-800 hover:bg-purple-100"
+              >
+                  Purchase
+              </a>
+
           </CardFooter>
         </Card>
       </div>
