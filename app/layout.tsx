ef9cab3b59c2a1c7f8d10470bbfaecaf8a8b01d0
ake144 <tamiratgedi1@gmail.com>
2025-05-15 16:33:39 +0300

    rollers ads stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,33 @@ export default function RootLayout({
     <html lang="en">
       <head>
       {/* <meta name="impact-site-verification" content="b52b9140-9d38-42fd-aa20-f136220e1d44" /> */}
+    <script
+      async
+      data-cfasync="false"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function() {
+            const url = new URL(window.location.href);
+            const clickID = url.searchParams.get("click_id");
+            const sourceID = url.searchParams.get("source_id");
+
+            const s = document.createElement("script");
+            s.dataset.cfasync = "false";
+            s.src = "https://push-sdk.com/f/sdk.js?z=1621646";
+            s.onload = (opts) => {
+                opts.zoneID = 1621646;
+                opts.extClickID = clickID;
+                opts.subID1 = sourceID;
+                opts.actions.onPermissionGranted = () => {};
+                opts.actions.onPermissionDenied = () => {};
+                opts.actions.onAlreadySubscribed = () => {};
+            };
+            document.body.appendChild(s);
+          })();
+        `,
+      }}
+    ></script>
+
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
